063eb72e07bc2b7a075805856ca8d6f6e35dfa47
FrankXie2012 <fr6nkxie@gmail.com>
2017-11-12 23:58:26 +0800

    Index page.

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,29 @@ export {
 	carousel
 };
 
+// const init = () => {
+//     $('html').removeClass('hidden');
+// };
+
 const carousel = () => {
 	$('.carousel').unslider({
 		autoplay: true,
 		animation: 'fade',
 		arrows: false
 	});
+	$('.v-carousel').unslider({
+		autoplay: true,
+		animation: 'vertical',
+		infinite: true,
+		nav: false,
+		arrows: false
+	});
+	$('.card-carousel').unslider({
+		autoplay: true,
+		nav: false,
+		arrows: false
+	});
 };
 
+// init();
 carousel();
