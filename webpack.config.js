5e227c8afffa70705db8553f9589912e83378727
FrankXie2012 <fr6nkxie@gmail.com>
2017-11-28 00:35:50 +0800

    index page.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
-        index: 'src/js/index.js'
+        index: __dirname + '/src/js/index.js'
     },
     output: {
         // filename: 'bundle-[hash].js',
